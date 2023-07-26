057921d7e7ab0f22e86ccd4f4c65fc9327528711
Ajay Mandal <68213789+0xAjayMandal@users.noreply.github.com>
2023-07-26 16:35:19 +0530

    Time: 136 ms (56.71%) | Memory: 16.6 MB (8.42%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,7 +5,7 @@ class Solution:
         left = 0
         for right in range(len(s)):
             hashmap[s[right]] = 1 + hashmap.get(s[right], 0)
-            while (right-left+1) - max(hashmap.values()) > k:
+            if (right-left+1) - max(hashmap.values()) > k:
                 hashmap[s[left]] -= 1
                 left += 1
             result = max(result, right-left+1)
