dc530a0d234a24b118e7444a29651b5a38c16b44
Ajay Mandal <68213789+0xAjayMandal@users.noreply.github.com>
2023-07-27 18:49:34 +0530

    Time: 27 ms (99.91%) | Memory: 16.4 MB (14.94%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        arr = []
+        for i in s:
+            if i =="(" or i =="{" or i=="[":
+                arr.append(i)
+            elif len(arr) == 0:
+                return False
+            elif (i == ")" and arr[-1]=="(") or (i == "}" and arr[-1]=="{") or(i == "]" and arr[-1]=="["):
+                arr.pop()
+            else:
+                return False
+        if len(arr) == 0:
+            return True
+        else:
+            return False
