9ecaf43997df62c5a41da8da155105d39d728bef
Ajay Mandal <68213789+0xAjayMandal@users.noreply.github.com>
2023-07-28 15:16:51 +0530

    Time: 38 ms (99.86%) | Memory: 17.7 MB (29.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         num1 = "".join(x for x in s if x.isalnum())
-        num1 = num1.lower()
         num2 = "".join(reversed(num1))
-        return num1 == num2
+        return num1.lower() == num2.lower()
