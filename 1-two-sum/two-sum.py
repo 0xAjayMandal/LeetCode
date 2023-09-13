1e96f6eb757494494627691636b873613e4354d6
Ajay Mandal <68213789+ajay-mandal@users.noreply.github.com>
2023-09-13 12:43:27 +0530

    Time: 2469 ms (31.94%) | Memory: 17.1 MB (90.71%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        reverse_table = dict()
-        for i in range(len(nums)):
-            second_target = target - nums[i]
-            if second_target in reverse_table:
-                return [reverse_table[second_target],i]
-            else:
-                reverse_table[nums[i]] =i
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+        
