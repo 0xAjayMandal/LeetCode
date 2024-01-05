816d888adf899e72d07840ba025fb38606073d2f
Ajay Mandal <68213789+ajay-mandal@users.noreply.github.com>
2024-01-05 11:29:15 +0530

    Time: 54 ms (90.45%) | Memory: 17.5 MB (11.42%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        newArray =[]
+        for i in range(n):
+            newArray.append(nums[i])
+            newArray.append(nums[i+n])
+        return newArray
