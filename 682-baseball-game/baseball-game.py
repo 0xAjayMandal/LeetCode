d159e2c15a8d122f2a37dbb2dcd66c6acb1a767b
Ajay Mandal <68213789+ajay-mandal@users.noreply.github.com>
2024-02-21 17:28:47 +0530

    Time: 46 ms (50.75%) | Memory: 16.8 MB (82.69%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,7 +1,7 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
-
+        sum1 = 0
         for op in operations:
             if op == '+':
                 stack.append(stack[-1]+stack[-2])
@@ -11,4 +11,6 @@ class Solution:
                 stack.pop()
             else:
                 stack.append(int(op))
-        return sum(stack)
+        for i in range(len(stack)):
+            sum1 +=  stack[i]
+        return sum1
